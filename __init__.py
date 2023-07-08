1b11e89e48e2406884635c37dec4a1b3fdb52d74
PabloPatin <pashka.lop@mail.ru>
2023-07-08 21:34:44 +0300

    correct __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
 import os
 
-with open('path.py', 'w', encoding='utf-8') as file:
-    file.write(f"ABS_PATH = '{os.path.dirname(os.path.abspath(__file__))}'")
+path = os.path.dirname(os.path.abspath(__file__)).replace('\\', '\\\\')
+print(path)
+with open(f'{path}\\path.py', 'w', encoding='utf-8') as file:
+    file.write(f"ABS_PATH = '{path}'")
+    print('lol')
